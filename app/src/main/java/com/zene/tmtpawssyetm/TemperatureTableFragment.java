134c6651b285cd3a24e5ffd4799940f22fce3911
master <efrenguarinmartinez@gmail.com>
2022-03-31 18:09:31 +0800

    Semi-full version

--- app/src/main/java/com/zene/tmtpawssyetm/TemperatureTableFragment.java
+++ app/src/main/java/com/zene/tmtpawssyetm/TemperatureTableFragment.java
@@ -7,16 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link TemperatureTableFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class TemperatureTableFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,9 +24,7 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private Button btnTemperature;
-
-    public HomeFragment() {
+    public TemperatureTableFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +34,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment TemperatureTableFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static TemperatureTableFragment newInstance(String param1, String param2) {
+        TemperatureTableFragment fragment = new TemperatureTableFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,22 +59,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        btnTemperature = view.findViewById(R.id.btnTemperature);
-
-        btnTemperature.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                TemperatureFragment temperatureFragment = new TemperatureFragment();
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(((ViewGroup)getView().getParent()).getId(), temperatureFragment, "findThisFragment")
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_temperature_table, container, false);
     }
 }
